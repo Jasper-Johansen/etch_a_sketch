f23aa2cee04ff6151f152e903e7e70edd8e0d18f
Jasper_Johansen <anoopanoop2001@gmail.com>
2025-03-18 13:43:30 +0530

    -shift css styles to style.css

--- script.js
+++ script.js
@@ -4,11 +4,7 @@ for (let i=1; i<=16*16; i++){
     const square = document.createElement("div");
     square.classList.add("square");
     container.appendChild(square);
-    square.style.cssText = `border:1px solid pink; 
-                            height:30px; 
-                            width:30px; 
-                            margin:2px;`;
-
+    
     square.addEventListener("mouseover",()=>{
         square.style.backgroundColor = `rgb(${Math.floor(Math.random()*256)},
                                 ${Math.floor(Math.random()*256)},
