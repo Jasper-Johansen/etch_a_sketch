2b5ca6eab26750c6df287f55669f82bbd68c7806
Jasper_Johansen <anoopanoop2001@gmail.com>
2025-03-18 15:48:16 +0530

    implement any no. of squares in the fixed width

--- script.js
+++ script.js
@@ -19,15 +19,15 @@ btn.addEventListener("click",()=>{
     let n = parseInt(prompt("Enter the number of rows and columns for the grid!"));
     container.innerHTML = "";    /*Clears the container elements when the button is clicked"*/
 
-    if(n>100 || n==0){
+    if(n>100 || n == 0 || n == ''){
         alert("Please enter a number between 1 and 100");
     }
     else{
         for (let j=1; j<=n*n; j++){
-            container.style.cssText=`width:${n*30}px;
-                                    height:${n*30}px;`
             const square = document.createElement("div");
             square.classList.add("square");
+            square.style.cssText = `width:${480/n}px;
+                                    height:${480/n}px`
             container.appendChild(square);
             
             square.addEventListener("mouseover",()=>{
