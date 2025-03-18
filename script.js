3a61644da48fc53000a5c5c563463ddbb1798bd5
Jasper_Johansen <anoopanoop2001@gmail.com>
2025-03-18 11:34:10 +0530

    add random colour hovering using mouseover evenListener

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ const container = document.querySelector(".container");
 
 for (let i=1; i<=16*16; i++){
     const square = document.createElement("div");
+    square.classList.add("square");
     container.appendChild(square);
-    square.style.cssText = "border:1px solid pink; height:30px; width:30px; gap:10px;";
-}
+    square.style.cssText = `border:1px solid pink; 
+                            height:30px; 
+                            width:30px; 
+                            margin:2px;`;
+
+    square.addEventListener("mouseover",()=>{
+        square.style.backgroundColor = `rgb(${Math.floor(Math.random()*256)},
+                                ${Math.floor(Math.random()*256)},
+                                ${Math.floor(Math.random()*256)}`;
+    });
+    
+        };
+
